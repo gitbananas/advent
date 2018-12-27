42ddcd54723532f3053ae0fb64782a5275b4c556
austingerig <austingerig@gmail.com>
2018-12-27 14:48:16 -0500

    Final Day2 code

--- day2/day2.py
+++ day2/day2.py
@@ -17,3 +17,15 @@ with open('input.txt','r') as input_file:
   print('Number of boxes with 2 repeated letters = ', num2)
   print('Number of boxes with 3 repeated letters = ', num3)
   print('Checksum = ', num2*num3)
+  
+#find boxes with IDs that differ by only one character
+length_ID = len(box_IDs[1])
+found_boxes=[]
+for i,box1 in enumerate(box_IDs):
+  for j,box2 in enumerate(box_IDs):
+    if i!=j:
+      num_dupes = sum(c1==c2 for c1,c2 in zip(box1,box2))
+      if num_dupes == length_ID - 1:
+        found_boxes.append([i,box1])
+print(found_boxes)
+        
